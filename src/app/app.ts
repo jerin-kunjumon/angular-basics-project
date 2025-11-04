a9a1469a8b4a9d7dc3728b01d6d8e2aa7ad6e315
jerin-kunjumon <jkjerryomalloor@gmail.com>
2025-11-04 16:25:30 +0530

    Counter message

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import ProfileComponent from './profile.compnent';
+import CounterComponent from './Counter/Counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, ProfileComponent, CounterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-basics');
+  counter: number = 20;
 }
